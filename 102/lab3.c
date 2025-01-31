20695897cee1888a84012167938c1c0722d390df
thowley <thowley@joey7.computing.clemson.edu>
2025-01-31 12:16:54 -0500

    initial commit

---
+++ 102/lab3.c
@@ -0,0 +1,33 @@
+/* lab3.c
+   Thomas Howley
+   CpSc 1011, Section # , Spring 2025
+   Due 1/31/25
+   Lab #3
+   Outputs
+*/
+#include "stdio.h"
+#include "math.h"
+int main(void) {
+   char input;
+   int ascii;
+   double squareR;
+   int prec;
+
+
+   fprintf(stderr, "Enter any character from the keyboard.\n");
+   scanf("%c",&input);
+
+
+   ascii = (int)input;
+   squareR = sqrt(ascii);
+   printf("The ASCII value is: %d.\n ",input);
+
+   printf("The square root value of '%c' is : %0.1f.\n",input,squareR);
+
+   prec = (int)squareR;
+
+   printf("The value of PI is : %.*f.\n",prec, M_PI);
+
+   return 0;
+}	
+
